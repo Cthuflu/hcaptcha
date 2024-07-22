882d20fa241052a01eb7980a60833929c7034878
Jacob Pfeiffer <jacob@pfeiffer.codes>
2024-07-22 15:53:59 -0400

    Correct docs and urls

--- lib/hcaptcha/template.ex
+++ lib/hcaptcha/template.ex
@@ -2,9 +2,6 @@ defmodule Hcaptcha.Template do
   @moduledoc """
     Responsible for rendering boilerplate hcaptcha HTML code, supports noscript fallback.
 
-    [Some](https://developers.google.com/hcaptcha/docs/display#explicit_render)
-    functionality is not currently supported.
-
     In future this module may be separated out into a Phoenix specific library.
   """
   require Elixir.EEx
